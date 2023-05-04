6a3553238032eaae9f24f716e3a4f2e619044e32
VINICIUS CARVALHO DE SOUZA <viniciuscarvalhomuay@gmail.com>
2023-05-04 16:07:50 -0300

    terminado

--- js/script.js
+++ js/script.js
@@ -1,35 +1,38 @@
 function menuMobile() {
   const button = document.querySelector(".container");
-  const nav = document.querySelector(".navegacao");
+  const menu = document.querySelector(".navegacao");
 
-  button.addEventListener("touchstart", btnClick);
-  button.addEventListener("click", btnClick);
-  function btnClick() {
+  button.addEventListener("click", handleButtonClick);
+  button.addEventListener("touchstart", handleButtonClick);
+
+  function handleButtonClick(event) {
+    if (event.type === "touchstart") event.preventDefault();
+    event.stopPropagation();
     this.classList.toggle("change");
     this.classList.toggle("active");
-    nav.classList.toggle("active");
-  }
-  nav.addEventListener("click", navClick);
-  nav.addEventListener("touchstart", navClick);
-  function navClick(event) {
-    event.stopPropagation();
-    outsideClick(this, () => {
-      this.classList.remove("active");
-      button.classList.remove("change");
+    menu.classList.toggle("active");
+
+    handleClickOutside(menu, () => {
+      menu.classList.remove("active");
       button.classList.remove("active");
+      button.classList.remove("change");
     });
   }
 
-  function outsideClick(element, callback) {
+  function handleClickOutside(element, callback) {
     const html = document.documentElement;
-    html.addEventListener("click", handleOutsideClick);
-    html.addEventListener("touchstart", handleOutsideClick);
-    function handleOutsideClick(event) {
+    function handleHtmlClick(event) {
       if (!element.contains(event.target)) {
-        html.removeEventListener("click", handleOutsideClick);
-        html.removeEventListener("touchstart", handleOutsideClick);
+        element.removeAttribute("data-target");
+        html.removeEventListener("click", handleHtmlClick);
+        html.removeEventListener("touchstart", handleHtmlClick);
         callback();
       }
+    }
+    if (!element.hasAttribute("data-target")) {
+      html.addEventListener("click", handleHtmlClick);
+      html.addEventListener("touchstart", handleHtmlClick);
+      element.setAttribute("data-target", "");
     }
   }
 }
